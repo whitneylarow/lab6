7884b0218e133eb69c5441d11f5bf1f9c54db856
vagrant <vagrant@precise32.(none)>
2014-02-12 23:33:52 +0000

    ajax lab

--- app.js
+++ app.js
@@ -6,7 +6,8 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var handlebars = require('express3-handlebars')
+var handlebars = require('express3-handlebars');
+var palette = require('./routes/palette');
 
 var index = require('./routes/index');
 var project = require('./routes/project');
@@ -38,6 +39,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
 
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -21,18 +21,54 @@ function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
 
+	$('wait').show();
+
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProj);
+
+	console.log("Calling URL: " + "/project/" + idNumber);
+
 	console.log("User clicked on project " + idNumber);
 }
+
+function addProj(result) {
+	console.log(result);
+	var projectHTML =
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p>' + '<p>' + result['summary'] + '</p>'; ;
+
+	console.log($("#project" + result.id + " .details"));
+	$("#project" + result.id + " .details").html(projectHTML);
+}
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", addPalette);
+
+
 	console.log("User clicked on color button");
 }
+
+
+
+
+function addPalette(result) {
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
